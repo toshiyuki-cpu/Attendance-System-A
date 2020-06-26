17d0dcef9ba7999c5830be68cc70b880235ac9b2
toshiyuki-cpu <spd36bq9@gmail.com>
2020-06-26 10:09:44 +0000

    titleの変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def full_title(page_name = "") #full_titleヘルパーメソッド
+    base_title = "AttendanceApp"
+    if page_name.empty? #引数を受け取っているか判定
+      base_title
+    else
+      page_name + " | " + base_title
+    end
+  end
 end
