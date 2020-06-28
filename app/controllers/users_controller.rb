09e2384412dfc46e82611cba764643862edb4491
toshiyuki-cpu <spd36bq9@gmail.com>
2020-06-28 15:03:37 +0000

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  def create
+    @user = User.new(user_params) #(params[:user]) userコントローラーにシンボルparams[:user]として渡している
+    if @user.save
+      flash[:success] = '新規作成に成功しました。' #:successというキーには保存に成功した時のメッセージを代入
+      redirect_to @user #左記のように記述できる　redirect_to user_url(@user)
+    else
+      render :new
+    end
+  end
   
+  private #Web経由で外部のユーザーが知る必要は無いため、次に記すようにRubyのprivateキーワードを用いて外部からは使用できないようにする
+  
+  def user_params #このメソッドは前述したparams[:user]の代わり
+    params.require(:user).permit(:name, :email, :password, :password_confirmation) #Storong Parameter
+    #必須となるパラメータと許可されたパラメータを指定することができる
+    #paramsハッシュでは:userキーを必須とする
+  end
 end
