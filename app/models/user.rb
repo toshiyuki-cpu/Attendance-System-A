14f1c6a3ccfa107dc1904afbfd036e88f472c1d7
toshiyuki-cpu <spd36bq9@gmail.com>
2020-06-28 12:22:01 +0000

    新規作成ページ

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord #Userモデル
+# before_saveとはActive Recordのコールバックメソッド
   before_save { self.email = email.downcase } #現在のメールアドレス（self.email）の値をdowncaseメソッドを使って小文字に変換
   
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 } #presence 存在性
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i #正規表現を代入している（値が変化する事がない為定数として定義）
   
@@ -14,3 +15,9 @@ class User < ApplicationRecord #Userモデル
                       
   validates :password, presence: true, length: { minimum: 6 }
 end
+
+#has_secure_passwordを追加することで(bcryptと言うgemをインストール)
+#1.ハッシュ化したパスワードを、データベースのpassword_digestというカラムに保存できるようになる。
+#2.ペアとなる仮想的なカラムであるpasswordとpassword_confirmationが使えるようになる。さらに存在性と値が一致するかどうかの検証も追加される。
+#3.authenticateメソッドが使用可能となる。
+#このメソッドは引数の文字列がパスワードと一致した場合オブジェクトを返し、パスワードが一致しない場合はfalseを返す。
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   
   root 'static_pages#top'
   get  '/signup', to: 'users#new'
-
+  resources :users #4つの基本操作（POST GET PATCH DELETE）が定義されている
 end
