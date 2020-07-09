8f86e63d09cef40094b6e2ddb59548ce87a3f942
toshiyuki-cpu <spd36bq9@gmail.com>
2020-07-09 15:00:57 +0000

    基本情報更新モーダル

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy] # ログイン済みのユーザー
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info] # ログイン済みのユーザー
   before_action :correct_user, only: [:edit, :update] # アクセスしたユーザーが現在ログインしているユーザーか
-  before_action :admin_user, only: :destroy
+  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
   
   def index
     # @users = User.all 
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   end
   
   def show
-   # @user = User.find(params[:id])
+   # @user = User.find(params[:id]) set_userへ
   end
   
   def new
@@ -32,11 +32,11 @@ class UsersController < ApplicationController
   end
   
   def edit
-    # @user = User.find(params[:id])
+    # @user = User.find(params[:id]) set_userへ
   end
   
   def update
-    #@user = User.find(params[:id])
+    #@user = User.find(params[:id]) set_userへ
     if @user.update_attributes(user_params) 
       # user controllerのvalidationにallow_nil: trueオプションを追加するとパスワード入力しなくても更新できる
       flash[:success] = "ユーザー情報を更新しました。"
@@ -47,9 +47,18 @@ class UsersController < ApplicationController
   end
   
   def destroy
+    #@user = User.find(params[:id]) set_userへ
     @user.destroy
     flash[:success] = "#{@user.name}のデータを削除しました。"
     redirect_to users_url
+  end
+  
+  def edit_basic_info
+    #@user = User.find(params[:id]) set_userへ
+  end
+  
+  def update_basic_info
+    #@user = User.find(params[:id]) set_userへ
   end
   
   private #Web経由で外部のユーザーが知る必要は無いため、次に記すようにRubyのprivateキーワードを用いて外部からは使用できないようにする
