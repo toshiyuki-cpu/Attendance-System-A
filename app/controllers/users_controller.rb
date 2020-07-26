e8ae0abe18b61cbb8888a638b542629ee813690d
toshiyuki-cpu <spd36bq9@gmail.com>
2020-07-26 11:32:20 +0000

    ユーザー一覧から管理者除外

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
     #paginateではキーが:pageで値がページ番号のハッシュを引数にとります。
     #User.paginateは:pageパラメータに基づき、データベースからひとかたまりのデータを取得
     #@users = User.paginate(page: params[:page])　検索フォーム無しの場合
-    @users = User.paginate(page: params[:page]).search(params[:search])
+    #@users = User.paginate(page: params[:page]).search(params[:search]) 検索フォーム有りの場合
+    @users = User.paginate(page: params[:page]).search(params[:search]).where.not(admin: true) #where.not(admin: true)管理者を表示させない
   end
   
   def show
