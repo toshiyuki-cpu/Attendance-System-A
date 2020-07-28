3731ba5820e0c782dd670c74e0adc5a06fc2f3af
toshiyuki-cpu <spd36bq9@gmail.com>
2020-07-28 17:18:58 +0000

    拠点一覧画面作成

--- app/controllers/bases_controller.rb
+++ app/controllers/bases_controller.rb
@@ -1,2 +1,36 @@
 class BasesController < ApplicationController
+  
+  def index
+    @bases = Base.all
+    @base = Base.new
+  end
+  
+  def show
+  end
+  
+  def create
+    @base = Base.new(base_params)
+    if @base.save
+      flash[:success] = '#{@base.base_name}の拠点情報を追加しました。' #:successというキーには保存に成功した時のメッセージを代入
+      redirect_to bases_url #左記のように記述できる　redirect_to user_url(@user)
+    else
+      render :new
+    end
+  end
+  
+  def destroy
+    #@user = User.find(params[:id]) set_userへ
+    @base.destroy
+    flash[:success] = "#{@base.base_name}のデータを削除しました。"
+    redirect_to bases_url
+  end
 end
+
+private
+
+def base_params #このメソッドはparams[:base]の代わり
+    params.require(:base).permit(:base_number, :base_name, :base_type) #Storong Parameter
+    #必須となるパラメータと許可されたパラメータを指定することができる
+    #paramsハッシュでは:userキーを必須とする
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   
-  resources :users do #4つの基本操作（POST GET PATCH DELETE）が定義されている
+  resources :users, :bases do #4つの基本操作（POST GET PATCH DELETE）が定義されている
   
   member do #生成されたurlにuserを識別するための:idが自動で追加されます
     get 'attend_employees' #出勤中社員一覧
