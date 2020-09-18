4bbd5406464933e0ba79a4660d47d1324b1498b0
toshiyuki-cpu <spd36bq9@gmail.com>
2020-09-18 01:02:16 +0000

    9/17日を跨ぐ計算

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -27,14 +27,12 @@ module AttendancesHelper
     #format("%.2f", (day.overtime_work_end_plan.hour - @user.designated_work_end_time.hour) + 
 						#(day.overtime_work_end_plan.min - @user.designated_work_end_time.min) / 60.00)
   #end
-  def hours_of_overtime(end_time, end_plan, next_day)
-    #if day_check == "1" #チェック有りなら
-      #format("%.2f", (24 + end_plan.hour) - end_time.hour + (end_plan.min - end_time.min) / 60.00) 
-    #else
+  def hours_of_overtime(next_day, end_time, end_plan)
+    if next_day == "1" #チェック有りなら
+      format("%.2f", (24 + end_plan.hour) - end_time.hour + (end_plan.min - end_time.min) / 60.00) 
+    else
       format("%.2f", (end_plan.hour - end_time.hour) + (end_plan.min - end_time.min) / 60.00)
-        
-      #format("%.2f", (24 + end_plan.hour) - end_time.hour + (end_plan.min - end_time.min) / 60.00) 
-    #end
+    end
   end
 end  
 # Attendances Helperにより下記のshow.html.erbの記述はいらなくなる
