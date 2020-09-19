61f8811411ed9822684ac0b2f4f45a3fb9d20c2a
toshiyuki-cpu <spd36bq9@gmail.com>
2020-09-19 10:36:46 +0000

    ユーザー役割設定、残業申請画面で上司選択

--- db/migrate/20200918030834_add_role_to_users.rb
+++ db/migrate/20200918030834_add_role_to_users.rb
@@ -1,5 +1,5 @@
 class AddRoleToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :role, :string, default: 'employee', null: false, comment: '社員と上司区別'
+    add_column :users, :role, :string, default: 'employee', null: false, comment: '役割(社員/上司)'
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,25 @@
 # coding: utf-8
 
 # 管理者の作成
-User.create!(role: admin,
-             name: "Admin User",
+
+User.create!(name: "Admin User",
              email: "sample@email.com",
+             role: 'admin',
              password: "password",
              password_confirmation: "password",
              admin: true) #最初のユーザーだけadmin属性をtrueに設定
 
 # 上司Aの作成
-User.create!(role: superiorA,
-             name: "SuperiorA",
+User.create!(name: "SuperiorA",
              email: "superior-a@email.com",
+             role: 'superior',
              password: "password",
              password_confirmation: "password")
              
-# 上司Bの作成             
-User.create!(role: superiorB,
-             name: "SuperiorB",
+# 上司Bの作成
+User.create!(name: "SuperiorB",
              email: "superior-b@email.com",
+             role: 'superior',
              password: "password",
              password_confirmation: "password")
              
@@ -26,9 +27,9 @@ User.create!(role: superiorB,
   name  = Faker::Name.name
   email = "sample-#{n+1}@email.com"
   password = "password"
-  User.create!(role: employee,
-               name: name,
+  User.create!(name: name,
                email: email,
+               role: 'employee',
                password: password,
                password_confirmation: password)
 end
