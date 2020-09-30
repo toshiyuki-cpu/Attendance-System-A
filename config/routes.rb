4470806134846e7f9cfa2d0a6c69a4200dba4887
toshiyuki-cpu <spd36bq9@gmail.com>
2020-09-30 04:01:14 +0000

    インデント再修正

--- config/routes.rb
+++ config/routes.rb
@@ -14,24 +14,22 @@ Rails.application.routes.draw do
   end
 
   resources :users do #4つの基本操作（POST GET PATCH DELETE）が定義されている
-
-  member do #生成されたurlにuserを識別するための:idが自動で追加されます
-    get 'attend_employees' #出勤中社員一覧
-    get 'edit_basic-info' #ルーティング設定してアクションを定義
-    patch 'update_basic_info' #ルーティング設定してアクションを定義
-    get 'attendances/edit_one_month' #ルーティング設定してアクションを定義
-    patch 'attendances/update_one_month' #ルーティング設定してアクションを定義
-    get 'attendances/edit_log' #勤怠ログ
-    get 'attendances/edit_overtime_work_end_plan'
-    patch 'attendances/update_overtime_work_end_plan'
-  end
-
-  resources :attendances, only: :update # onlyオプションで指定することで、updateアクション以外のルーティングを制限できます
+    member do #生成されたurlにuserを識別するための:idが自動で追加されます
+      get 'attend_employees' #出勤中社員一覧
+      get 'edit_basic-info' #ルーティング設定してアクションを定義
+      patch 'update_basic_info' #ルーティング設定してアクションを定義
+      get 'attendances/edit_one_month' #ルーティング設定してアクションを定義
+      patch 'attendances/update_one_month' #ルーティング設定してアクションを定義
+      get 'attendances/edit_log' #勤怠ログ
+      get 'attendances/edit_overtime_work_end_plan'
+      patch 'attendances/update_overtime_work_end_plan'
+    end
+    
+    resources :attendances, only: :update # onlyオプションで指定することで、updateアクション以外のルーティングを制限できます
   # Usersリソースのブロック内に記述しているため、設定されるルーティングは
   # HTTP　PATCH
   # URL /users/:user_id/attendances/:id　　params[:user_id]でユーザーIDが取得できる
   # PATH user_attendance_path	
   # コントローラー#アクション　attendances#update　となる
-
+    end
   end
-end
